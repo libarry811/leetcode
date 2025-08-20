8d5b9ee910a332d2a011951e5b273b58fac48408
libarry811 <166108196+libarry811@users.noreply.github.com>
2025-08-20 17:06:43 +0800

    Update 203.cpp

--- 203.cpp
+++ 203.cpp
@@ -1,11 +1,12 @@
+//203移除链表元素
 /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *     ListNode *next;成员变量
+ *     ListNode() : val(0), next(nullptr) {}//第一个构造函数（默认构造函数）
+ *     ListNode(int x) : val(x), next(nullptr) {}//这是带一个int参数的构造函数，创建节点时传入一个值x时调用。
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}//这是带两个参数的构造函数，创建节点时传入一个值x和一个ListNode指针时调用。
  * };
  */
 class Solution {
