151784f6a9f1c862a5e6b6fe05e62354fba62995
libarry811 <166108196+libarry811@users.noreply.github.com>
2026-03-17 20:39:36 +0800

    Create 翻转二叉树.cpp

---
+++ hot100/C++/二叉树/翻转二叉树.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void reverse(TreeNode* root)//中序遍历
+    {
+        if(!root)
+        {
+            return;
+        }
+        reverse(root->left);
+        reverse(root->right);
+        if(root->left||root->right)
+        {
+            TreeNode* tmp;
+            tmp = root->left;
+            root->left = root->right;
+            root->right = tmp;
+        }
+
+    }
+    TreeNode* invertTree(TreeNode* root) 
+    {
+        reverse(root);
+        return(root);
+    }
+};
